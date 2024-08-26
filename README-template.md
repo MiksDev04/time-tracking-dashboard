0b072d6da1e7f9ab33d5a4d0706cb1b2cf1b9826
MiksDev04 <mikogapasan04@gmail.com>
2024-08-26 13:57:41 +0800

    Initial commit

---
+++ README-template.md
@@ -0,0 +1,66 @@
+# Frontend Mentor - Time tracking dashboard solution
+
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch between viewing Daily, Weekly, and Monthly stats
+
+### Screenshot
+
+![][./screenshot/desktop.png]
+![][./screenshot/active.png]
+![][./screenshot/mobile.png]
+
+
+### Links
+
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+
+
+### What I learned
+
+I learned to create a simple time tracking board.
+
+
+### Continued development
+
+I want to build more projects like this because it helps me to improve my designing skills
+
+## Author
+
+- Frontend Mentor - [@MiksDev04](https://www.frontendmentor.io/profile/Miksdev04)
+
+
+
+[./screenshot/desktop.png]: ./screenshot/desktop.png
+[./screenshot/active.png]: ./screenshot/active.png
+[./screenshot/mobile.png]: ./screenshot/mobile.png
---
+++ script.js
@@ -0,0 +1,61 @@
+// define elements
+const options = document.querySelectorAll('.option-list input');
+
+// get data from json file
+function getData() {
+    fetch('data.json')
+        .then(value => value.json())
+        .then(updateOption)
+        .catch(error => console.error(error));
+}
+getData();
+
+// update the option chosen by the user
+function updateOption(obj) {
+    options.forEach(option => {
+        if (option.checked) {
+            locateData(option.value, obj);
+        }
+    })
+}
+changeOption();
+
+// change the option
+function changeOption() {
+    options.forEach((option, index, array) => {
+        option.addEventListener('input', function() {
+            getData();
+        })
+    })
+}
+
+// put the data according to the option chosen
+function locateData(value, obj) {
+    const dashboards = document.querySelectorAll('.dashboards');
+    const dashboardTitle = document.querySelectorAll('.dashboard-title .labels span');
+    const currentTime = document.querySelectorAll('.dashboard-title .current');
+    const previousTime = document.querySelectorAll('.dashboard-title .previous');
+
+    console.log(value, obj)
+    obj.forEach( function(dataObj, index, array) {
+        if (value === 'daily') {
+            dashboardTitle[index].textContent = dataObj.title;
+            currentTime[index].textContent = `${dataObj.timeframes.daily.current}hrs`;
+            previousTime[index].textContent = `Last day -${dataObj.timeframes.daily.previous}hrs`;
+        } else if (value === 'weekly') {
+            dashboardTitle[index].textContent = dataObj.title;
+            currentTime[index].textContent = `${dataObj.timeframes.weekly.current}hrs`;
+            previousTime[index].textContent = `Last week -${dataObj.timeframes.weekly.previous}hrs`;
+        } else if (value === 'monthly') {
+            dashboardTitle[index].textContent = dataObj.title;
+            currentTime[index].textContent = `${dataObj.timeframes.monthly.current}hrs`;
+            previousTime[index].textContent = `Last month -${dataObj.timeframes.monthly.previous}hrs`;
+
+        }
+    })
+
+
+
+
+}
+
